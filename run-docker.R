d8fa1d63c46454e55a6bc43546638ed48bcfe3f6
harell <harel@loostiger.com>
2020-08-18 16:54:26 +1200

    build

--- run-docker.R
+++ run-docker.R
@@ -5,11 +5,12 @@ app <- c("airflowdocker", "Shawe82", "puckel")[3]
 source_path <- file.path(getwd(), app)
 
 # Run Docker --------------------------------------------------------------
+docker$kill_all_containers()
 docker$remove_dangling_images()
 with_dir(source_path, DockerCompose$new()$stop())
 with_dir(source_path, DockerCompose$new()$start())
 
 # Open UI -----------------------------------------------------------------
-# cat('Opening browser in ')
-# for(i in 7:1) {cat(i, '...', sep=''); Sys.sleep(1)}
+cat('Opening browser in ')
+for(i in 7:1) {cat(i, '...', sep=''); Sys.sleep(1)}
 with_dir(source_path, DockerCompose$new()$browse_url("webserver"))
