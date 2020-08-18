abdb4621bcb6ba952b5a7f1d03df3f5f31771c14
harell <harel@loostiger.com>
2020-08-18 16:16:29 +1200

    local run

--- run-docker.R
+++ run-docker.R
@@ -1,7 +1,7 @@
 # Setup -------------------------------------------------------------------
 pkgload::load_all(export_all = FALSE, helpers = FALSE)
 with_dir <- withr::with_dir
-app <- c("airflowdocker", "Shawe82", "apache")[3]
+app <- c("airflowdocker", "Shawe82", "puckel")[3]
 source_path <- file.path(getwd(), app)
 
 # Run Docker --------------------------------------------------------------
